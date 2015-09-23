1967d03bf2e19c7e92463247cc390cf42ae85818
Stujo <stujo@github.com>
2015-09-23 11:55:33 -0700

    Add Image Preload

--- public/javascript/jumper.js
+++ public/javascript/jumper.js
@@ -1,11 +1,18 @@
 window.onload = function() {
-    debugger;
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create });
+    
+    var game = new Phaser.Game(400, 400, Phaser.AUTO, 'game', { 
+      preload: preload, create: create 
+    });
+    
     function preload () {
-        game.load.image('star', 'assets/star.png');
+      game.load.image('sky', 'assets/sky.png');
+      game.load.image('ground', 'assets/platform.png');
+      game.load.image('star', 'assets/star.png');
+      game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     }
+    
     function create () {
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'star');
         logo.anchor.setTo(0.5, 0.5);
     }
 };
